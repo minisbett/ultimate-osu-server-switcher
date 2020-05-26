8dd8b94034b0e2df3991e7e68712c59685029e6f
Niklas Fehde <eisenerzyt@gmail.com>
2020-05-26 18:45:09 +0200

    minor thing

--- UltimateOsuServerSwitcher/MainForm.cs
+++ UltimateOsuServerSwitcher/MainForm.cs
@@ -87,10 +87,8 @@ namespace UltimateOsuServerSwitcher
     private void LblGithub_Click(object sender, EventArgs e) =>
       Process.Start("http://www.github.com/minisbett/ultimate-osu-server-switcher");
 
-    private void btnUpdateAvailable_Click(object sender, EventArgs e)
-    {
-      Process.Start("https://github.com/MinisBett/ultimate-osu-server-switcher/releases");
-    }
+    private void btnUpdateAvailable_Click(object sender, EventArgs e) =>
+      Process.Start("https://github.com/MinisBett/ultimate-osu-server-switcher/releases/latest");
 
     private void pctrbxServerIcon_Click(object sender, EventArgs e)
     {
